268e76fef7768222e4b59567843e9f62682e978d
Kai Tindall <kaitindall2000@gmail.com>
2021-01-23 15:14:15 +0000

    Started unit testing

---
+++ Code/UnitTests/SecurityLib/SHA3HashingTests.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <SecurityLib/Implementaton/SHA3HashingService.hpp>
+
+TEST(SHA3Hashing, SunnyDay) {
+	// Arrange
+	std::string test_data = "Hello there";
+	securitylib::SHA3HashingService hashing_service;
+	std::string result;
+
+	// Act
+	result = hashing_service.Hash(test_data);
+
+	// Assert
+	std::string correct_result = "e034863118f611cae9ce9fe1c66e2e7bf750ccbd64f7fcb6a777b3d517332ac6";
+
+	ASSERT_EQ(result, correct_result);
+
+}
