21e0671bf46a098a2f12e9cfcccd3d708344a3b7
Kai Tindall <kaitindall2000@gmail.com>
2021-01-23 14:56:16 +0000

    Implemented SHA3

--- Code/SecurityLib/Services/SHA3HashingService.cpp
+++ Code/SecurityLib/Services/SHA3HashingService.cpp
@@ -1,8 +1,20 @@
 #include <string>
 #include <SecurityLib/Implementation/SHA3HashingService.hpp>
+#include "cryptlib.h"
+#include "sha3.h"
+
+// Code based on an example from https://www.cryptopp.com/wiki/SHA
 
 namespace securitylib {
 	std::string SHA3HashingService::Hash(std::string data) {
-		return "Not implemented";
+		CryptoPP::SHA3 hash;
+
+		std::string digest;
+
+		hash.Update((const byte*)data.data(), data.size());
+		digest.resize(hash.DigestSize());
+		hash.Final((byte*)&digest[0]);
+
+		return digest;
 	}
 }
