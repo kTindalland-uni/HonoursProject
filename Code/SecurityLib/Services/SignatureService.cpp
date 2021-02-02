2e18ce15c961c68ecb0355fe2e0ad74471663b06
Kai Tindall <kaitindall2000@gmail.com>
2021-02-02 14:45:38 +0000

    Written service, no tests

--- Code/SecurityLib/Services/SignatureService.cpp
+++ Code/SecurityLib/Services/SignatureService.cpp
@@ -1,13 +1,50 @@
 #include <string>
+#include <cryptlib.h>
+#include <osrng.h>
+#include <filters.h>
+#include <oids.h>
+#include <eccrypto.h>
 #include <SecurityLib/Implementation/SignatureService.hpp>
 
 namespace securitylib {
 	std::string SignatureService::SignData(std::string data, std::string private_key) {
-		return "Not Implemented";
+		// Load in private key
+		CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::PrivateKey privateKey;
+		CryptoPP::StringSource key_source(private_key, true);
+		privateKey.Load(key_source);
+
+		std::string signature;
+		CryptoPP::AutoSeededRandomPool prng;
+		CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::Signer signer(privateKey);
+
+		CryptoPP::StringSource s(data, true,
+				new CryptoPP::SignerFilter(prng,
+					signer,
+					new CryptoPP::StringSink(signature)
+				)
+			);
+
+		return signature;
 	}
 
 	bool CheckSignature(std::string signed_data, std::string public_key, std::string check_hash) {
-		return false;
+		// Load in public key
+		CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::PublicKey publicKey;
+		CryptoPP::StringSource key_source(public_key, true);
+		publicKey.Load(key_source);
+
+		CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::Verifier verifier(publicKey);
+
+		bool result = false;
+
+		CryptoPP::StringSource ss(signed_data+check_hash, true,
+				new CryptoPP::SignatureVerificationFilter(
+					verifier,
+					new CryptoPP::ArraySink( (CryptoPP::byte*)&result, sizeof(result) )
+				)
+			);
+
+		return result;
 	}
 }
 
