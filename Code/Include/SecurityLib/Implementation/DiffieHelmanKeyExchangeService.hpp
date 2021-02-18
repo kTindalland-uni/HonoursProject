29fdef82d070b998ec2490da64c24320af62ce71
Kai Tindall <kaitindall2000@gmail.com>
2021-02-18 14:28:19 +0000

    Part way through Diffie-Helman

--- Code/Include/SecurityLib/Implementation/DiffieHelmanKeyExchangeService.hpp
+++ Code/Include/SecurityLib/Implementation/DiffieHelmanKeyExchangeService.hpp
@@ -2,12 +2,16 @@
 #define __DIFHELHEYEXSERV
 
 #include <SecurityLib/Interfaces/IKeyExchangeService.hpp>
+#include <tuple>
+#include <integer.h>
 
 namespace securitylib {
 	class DiffieHelmanKeyExchangeService: public IKeyExchangeService {
 		public:
 			int GenerateIntermediateKey(int common_secret, int private_secret);
 			int GenerateFinalKey(int swapped_secret, int private_secret);
+		private:
+			std::tuple<CryptoPP::Integer, CryptoPP::Integer, CryptoPP::Integer, CryptoPP::Integer> GenerateInitialParameters();
 	};
 }
 
--- Code/SecurityLib/Services/DiffieHelmanKeyExchangeService.cpp
+++ Code/SecurityLib/Services/DiffieHelmanKeyExchangeService.cpp
@@ -1,7 +1,44 @@
+#include <tuple>
 #include <SecurityLib/Implementation/DiffieHelmanKeyExchangeService.hpp>
+#include <dh.h>
+#include <cryptlib.h>
+#include <osrng.h>
+#include <integer.h>
 
 namespace securitylib {
+	std::tuple<CryptoPP::Integer, CryptoPP::Integer,CryptoPP::Integer, CryptoPP::Integer> DiffieHelmanKeyExchangeService::GenerateInitialParameters() {
+		CryptoPP::DH dh;
+		CryptoPP::AutoSeededRandomPool prng;
+
+		dh.AccessGroupParameters().GenerateRandomWithKeySize(prng, 3072);
+
+		const CryptoPP::Integer p = dh.GetGroupParameters().GetModulus();
+		CryptoPP::Integer q = (p-1)/2;
+		const CryptoPP::Integer g = dh.GetGroupParameters().GetGenerator();
+		CryptoPP::Integer r = dh.GetGroupParameters().GetSubgroupOrder();
+
+
+		auto result = std::make_tuple(p, g, q, r);
+
+		return result;
+	}
+
 	int DiffieHelmanKeyExchangeService::GenerateIntermediateKey(int common_secret, int private_secret) {
+		// Get / Generate Parameters
+		// Just generating for now
+		auto params = GenerateInitialParameters();
+		CryptoPP::Integer p, g, q, r;
+		std::tie (p, g, q, r) = params;
+
+		
+		CryptoPP::AutoSeededRandomPool prng;
+		CryptoPP::DH dh;
+		dh.AccessGroupParameters().Initialize(p, q, g);
+
+		CryptoPP::SecByteBlock private_key(dh.PrivateKeyLength());
+		CryptoPP::SecByteBlock public_key(dh.PublicKeyLength());
+		dh.GenerateKeyPair(prng, private_key, public_key);
+
 		return 0;
 	}
 
