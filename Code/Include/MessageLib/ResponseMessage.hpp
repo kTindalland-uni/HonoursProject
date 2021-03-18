a665041c760004a3be9d402bf6404e9b2ec3cd09
Kai Tindall <kaitindall2000@gmail.com>
2021-03-18 10:13:45 +0000

    Response message created

---
+++ Code/Include/MessageLib/ResponseMessage.hpp
@@ -0,0 +1,16 @@
+#include <MessageLib/IMessage.hpp>
+#include <string>
+
+namespace msglib {
+    class ResponseMessage: public IMessage {
+        public:
+            std::string requestSent;
+            std::string response;
+
+            ResponseMessage();
+            ResponseMessage(std::string requestString, std::string responseString);
+
+            void Pack(unsigned char* buffer);
+            void Unpack(unsigned char* buffer);
+    };
+}
